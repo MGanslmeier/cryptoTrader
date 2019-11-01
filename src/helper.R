0625eedb576e92290ea5933de55ffe0cb1fb69e6
Michael Ganslmeier <m.ganslmeier@zeppelin-university.net>
2019-11-01 22:17:24 +0000

    update readme and trading function

--- src/helper.R
+++ src/helper.R
@@ -7,7 +7,15 @@ exeTweetDownloader <- function(coinNames){
 }
 
 # function to buy coin
-buyExe <- function(coinID, budgetShare, pair = pair){
+exeTrader <- function(coinID, holdingTime, budgetShare){
+
+    # Create objects and order book
+    orderBook <- data.frame('orderType'= character(), 'orderPair' = character(), 'orderTime'= character(),
+                            'orderPrice'= numeric(), 'orderAmount'= numeric(), stringsAsFactors = F)
+    pair <- paste('BTC_', coinID, sep = '')
+    buySuccess <- FALSE
+
+    ###################
 
     # Listen to file update
     initTweet <- file.size('data/tweets.json')
@@ -18,56 +26,43 @@ buyExe <- function(coinID, budgetShare, pair = pair){
         }
     }
 
+    ###################
+
     # Execute buy order
     rate <- ReturnTradeHistory(theObject = poloniex.public, pair = pair) %>%
         as.data.frame(.) %>% subset(.,type=='buy') %>% select(rate) %>% .[[1]] %>%
         as.character(.) %>% as.numeric(.) %>% max(.)
     buyAmount <- (ReturnBalances(poloniex.trading)[['BTC']] * budgetShare)/rate
     buySuccess <- ProcessTradingRequest(poloniex.trading, command = poloniex.trading@commands$buy,
-                                         args = list(currencyPair = pair, rate = rate, amount = buyAmount, immediateOrCancel = 1))
+                                        args = list(currencyPair = pair, rate = rate, amount = buyAmount, immediateOrCancel = 1))
     buySuccess <- as.numeric(buySuccess$amountUnfilled) == 0
 
     # Create entry in order book
     print(paste('Bought: ', pair, ' ', buyAmount, ' at ', Sys.time(), ' with rate ', rate, sep=''))
-    orderBook <<- rbind(orderBook, c('orderType'= 'buy', 'orderPair' = pair, 'orderTime'= Sys.time(),
-                                     'orderPrice'= rate, 'orderAmount'= buyAmount))
-}
+    temp <- data.frame(orderType = 'sell', 'orderPair' = pair, 'orderTime'= Sys.time(),
+                       'orderPrice'= rate, 'orderAmount'= buyAmount, stringsAsFactors = F)
+    orderBook <<- rbind(orderBook, temp)
 
-# function to sell coin
-sellExe <- function(coinID, pair = pair){
+    ###################
 
-    # Execute sell order
-    rate <- ReturnTradeHistory(theObject = poloniex.public, pair = pair) %>%
-        as.data.frame(.) %>% subset(.,type=='buy') %>% select(rate) %>% .[[1]] %>%
-        as.character(.) %>% as.numeric(.) %>% min(.)
-    sellAmount <- ReturnBalances(poloniex.trading)[[coinID]]
-    ProcessTradingRequest(poloniex.trading, command = poloniex.trading@commands$sell,
-                          args = list(currencyPair = pair, rate = rate, amount = sellAmount))
-
-    # Create entry in order book
-    print(paste('Sold: ', pair, ' ', sellAmount, ' at ', Sys.time(), ' with rate ', rate, sep=''))
-    orderBook <<- rbind(orderBook, c('orderType'= 'sell', 'orderPair' = pair, 'orderTime'= Sys.time(),
-                                     'orderPrice'= rate, 'orderAmount'= sellAmount))
-}
-
-# function to trade
-exeTrader <- function(coinID, holdingTime, budgetShare){
-
-    # Create objects and order book
-    orderBook <- data.frame('orderType'= character(), 'orderPair' = character(), 'orderTime'= character(),
-                            'orderPrice'= numeric(), 'orderAmount'= numeric(), stringsAsFactors = F)
-    pair <- paste('BTC_', coinID, sep = '')
-    buySuccess <- FALSE
-
-    # Execute buy and sell functions
-    buyExe(coinID = coinID, budgetShare = budgetShare)
     if(buySuccess ==  T){
 
         # Wait holdingTime
         Sys.sleep(holdingTime)
 
-        # Execute sell function
-        sellExe(coinID = coinID)
+        # Execute sell
+        rate <- ReturnTradeHistory(theObject = poloniex.public, pair = pair) %>%
+            as.data.frame(.) %>% subset(.,type=='buy') %>% select(rate) %>% .[[1]] %>%
+            as.character(.) %>% as.numeric(.) %>% min(.)
+        sellAmount <- ReturnBalances(poloniex.trading)[[coinID]]
+        ProcessTradingRequest(poloniex.trading, command = poloniex.trading@commands$sell,
+                              args = list(currencyPair = pair, rate = rate, amount = sellAmount))
+
+        # Create entry in order book
+        print(paste('Sold: ', pair, ' ', sellAmount, ' at ', Sys.time(), ' with rate ', rate, sep=''))
+        temp <- data.frame(orderType = 'sell', 'orderPair' = pair, 'orderTime'= Sys.time(),
+                           'orderPrice'= rate, 'orderAmount'= sellAmount, stringsAsFactors = F)
+        orderBook <<- rbind(orderBook, temp)
 
         # Reset buySuccess
         buySuccess <- FALSE
